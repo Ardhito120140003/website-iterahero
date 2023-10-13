9905c5ea9b81be56742b7e05ae746d5756f2913a
Revanfz <revanfauzi81@gmail.com>
2023-10-13 10:31:49 +0700

    Fix: Dropdown dashboard

--- src/page/dashboard/dashboard_operator.js
+++ src/page/dashboard/dashboard_operator.js
@@ -31,35 +31,19 @@ import CardDashboard from "../../component/card_dashboard/card_dashboard";
 import dashboardMenu from "../../Utility/dashboard_menu";
 import { json, useParams } from "react-router-dom";
 import { Field, Form, Formik } from "formik";
+import CardSensor from "../../component/card_sensor/card_sensor";
 
 const DashboardOperator = () => {
   TabTitle("Dashboard - ITERA Hero");
   const base_url = useSelector(selectUrl);
   const [dataApiDashboard, setDataApiDashboard] = useState(null);
-  const [dataApiSensor, setDataApiSensor] = useState(null);
   const [dataApiPenjadwalan, setDataApiPenjadwalan] = useState(null);
-  const [firstFilter, setFirstFilter] = useState(null)
-  const [filterData, setFilterData] = useState([])
+  const [firstFilter, setFirstFilter] = useState(null);
+  const [filterData, setFilterData] = useState([]);
   const [data, setData] = useState([]);
 
   const [action, setAction] = useState(false);
   const headers = localStorage.getItem("token");
-
-  useEffect(() => {
-    if (firstFilter) {
-      axios.get(base_url + "api/v1/" + firstFilter, {
-        headers: {
-          Authorization: "Bearer " + headers
-        }
-      })
-        .then(response => {
-          setFilterData(response.data.data)
-        })
-        .catch(err => {
-          console.error(err)
-        })
-    }
-  }, [firstFilter])
 
   const getApiPenjadwalan = () => {
     axios
@@ -93,26 +77,31 @@ const DashboardOperator = () => {
       });
   };
 
-  const getApiSensor = async () => {
-    axios
-      .get(base_url + "api/v1/sensor", {
-        headers: {
-          Authorization: `Bearer ${headers}`,
-        },
-      })
-      .then((response) => {
-        setDataApiSensor(response.data.data);
-        console.log(response.data.data);
-      })
-      .catch((error) => {
-        console.log("error", error);
-      });
-  };
+  useEffect(() => {
+    if (firstFilter) {
+      axios
+        .get(base_url + "api/v1/" + firstFilter, {
+          headers: {
+            Authorization: "Bearer " + headers,
+          },
+        })
+        .then((response) => {
+          console.log(response.data.data);
+          setFilterData(response.data.data);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  }, [firstFilter]);
 
   useEffect(() => {
     getApiDashboard();
     getApiPenjadwalan();
-    // getApiSensor();
+
+    // setTimeout(() => {
+    //   setAction(!action)
+    // }, 1500)
   }, [action]);
 
   return (
@@ -127,7 +116,7 @@ const DashboardOperator = () => {
             src="https://res.cloudinary.com/diyu8lkwy/image/upload/v1663542541/itera%20herro%20icon/Frame_181_fmtxbh.png"
             alignSelf={"center"}
           />
-          <Wrap mt={8} justify={{md:"center", sm:"center"}} spacing={30}>
+          <Wrap mt={8} justify={{ md: "center", sm: "center" }} spacing={30}>
             <CardDashboard
               data={{
                 value: dataApiDashboard.greenhouse,
@@ -174,77 +163,92 @@ const DashboardOperator = () => {
                 alert(JSON.stringify(values.filter2));
               }}
             >
-            {({ values, setFieldValue }) => (
-              <Form>
-                <Flex alignItems={"space-between"}>
-                  <Select
-                  size={"lg"}
-                    name="filter1"
-                    as={Select}
-                    borderRadius={"10"}
-                    placeholder="Pilih Filter"
-                    width={"100%"}
-                    height={"5vh"}
-                    bg={"white"}
-                    _active={{ bg: "white" }}
-                    borderColor={"var(--color-border)"}
-                    fontSize={"var(--header-5)"}
-                    fontWeight={"normal"}
-                    color={"var(--color-primer)"}
-                    _hover={{ borderColor: "var(--color-border)" }}
-                    _focusWithin={{ borderColor: "var(--color-border)" }}
-                    mr={5}
-                    onChange={(e) => {
-                        setFirstFilter(e.target.value)
-                        setFieldValue("filter1", e.target.value)
+              {({ values, setFieldValue, resetForm }) => (
+                <Form>
+                  <Flex alignItems={"space-between"}>
+                    <Select
+                      size={"lg"}
+                      name="filter1"
+                      as={Select}
+                      borderRadius={"10"}
+                      placeholder="Pilih Filter"
+                      width={"100%"}
+                      height={"5vh"}
+                      bg={"white"}
+                      _active={{ bg: "white" }}
+                      borderColor={"var(--color-border)"}
+                      fontSize={"var(--header-5)"}
+                      fontWeight={"normal"}
+                      color={"var(--color-primer)"}
+                      _hover={{ borderColor: "var(--color-border)" }}
+                      _focusWithin={{ borderColor: "var(--color-border)" }}
+                      mr={5}
+                      value={values.filter1}
+                      onChange={(e) => {
+                        resetForm({ filter1: e.target.value, filter2: ""})
+                        setFieldValue("filter1", e.target.value);
                       }}
-                  >
-                    <option value="greenhouse">Greenhouse</option>
-                    <option value="tandonUtama">Tandon</option>
-                  </Select>
-                  <Select
-                    name="filter2"
-                    as={Select}
-                    borderRadius={"10"}
-                    placeholder={values.filter1 ? 'Pilih ' + values.filter1 : '--'}
-                    width={"100%"}
-                    height={"5vh"}
-                    bg={"white"}
-                    _active={{ bg: "white" }}
-                    borderColor={"var(--color-border)"}
-                    fontSize={"var(--header-5)"}
-                    fontWeight={"normal"}
-                    color={"var(--color-primer)"}
-                    _hover={{ borderColor: "var(--color-border)" }}
-                    _focusWithin={{ borderColor: "var(--color-border)" }}
-                    onChange={async (e) => {
-                      setFieldValue("filter2", e.target.value)
-                      axios.get(base_url + "api/v1/" + values.filter1 + "/" + values.filter2 +"/sensor", {
-                        headers: {
-                          Authorization : "Bearer " + headers
-                        }
-                      })
-                      .then(response => {
-                        console.log(base_url + "api/v1/" + values.filter1 + "/" + values.filter2 +"/sensor")
-                        console.log(response.data)
-                        setData(response.data.data)
-                      })
-                      .catch(err => console.error(err))
-                    }}
-                  >
-                    {filterData.map((item, index) => (
-                      <option key={index} value={item.id}>
-                        {item.id}
-                      </option>
-                    ))}
-                  </Select>
-                </Flex>
-              </Form>
-            )}
-              
+                    >
+                      <option value="greenhouse">Greenhouse</option>
+                      <option value="tandonUtama">Tandon</option>
+                    </Select>
+                    <Select
+                      name="filter2"
+                      as={Select}
+                      borderRadius={"10"}
+                      width={"100%"}
+                      height={"5vh"}
+                      placeholder={values.filter1 ? "Pilih " + values.filter1 : "--"}
+                      bg={"white"}
+                      _active={{ bg: "white" }}
+                      borderColor={"var(--color-border)"}
+                      fontSize={"var(--header-5)"}
+                      fontWeight={"normal"}
+                      color={"var(--color-primer)"}
+                      _hover={{ borderColor: "var(--color-border)" }}
+                      _focusWithin={{ borderColor: "var(--color-border)" }}
+                      value={values.filter2}
+                      disabled={values.filter1 === ""}
+                      onChange={async (e) => {
+                        setFieldValue("filter2", e.target.value)
+                        axios
+                          .get(
+                            base_url +
+                              "api/v1/" +
+                              values.filter1 +
+                              "/" +
+                              e.target.value +
+                              "/sensor",
+                            {
+                              headers: {
+                                Authorization: "Bearer " + headers,
+                              },
+                            }
+                          )
+                          .then((response) => {
+                            console.log(response.data)
+                            setData(response.data.data);
+                          })
+                          .catch((err) => console.error(err));
+                      }}
+                    >
+                      {filterData.map((item, index) => (
+                        <option key={index} value={item.id}>
+                          {item.id}
+                        </option>
+                      ))}
+                    </Select>
+                  </Flex>
+                </Form>
+              )}
             </Formik>
           </Flex>
-          
+          <Box>
+            {data.map((item, index) => (
+              <Text key={index}>{JSON.stringify(item)}</Text>
+            ))}
+          </Box>
+
           {/* <Wrap>
             {selected === 1 && data !== "" ? (
               <CardSensor data={{ id: data }} />
